443b2a6ba780ae36cb6b9405a1808af5c2c8b124
kevingpqi <kevingpqi@tencent.com>
2021-01-20 13:24:01 -0500

    Push From Landun: update SDK

--- framework/libpag.framework/Headers/PAGMarker.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface PAGMarker : NSObject
-
-@property(nonatomic) long long startTime;
-@property(nonatomic) long long duration;
-@property(nonatomic, copy) NSString *comment;
-
-@end
--- framework/libpag.framework/Headers/PAGShapeLayer.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "PAGLayer.h"
-
-@interface PAGShapeLayer : PAGLayer
-
-
-@end
