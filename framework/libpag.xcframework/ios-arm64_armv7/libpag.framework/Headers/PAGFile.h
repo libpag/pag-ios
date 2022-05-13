4e5ffa5ea9de0226084db273e7e00b206331bf07
libpag <libpag@tencent.com>
2022-05-13 16:00:54 +0800

    Push From Landun: update SDK

--- framework/libpag.xcframework/ios-arm64_armv7/libpag.framework/Headers/PAGFile.h
+++ framework/libpag.xcframework/ios-arm64_armv7/libpag.framework/Headers/PAGFile.h
@@ -1,49 +1,46 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//  The MIT License (MIT)
+//  Tencent is pleased to support the open source community by making libpag available.
 //
-//  Copyright (c) 2016-present, Tencent. All rights reserved.
+//  Copyright (C) 2021 THL A29 Limited, a Tencent company. All rights reserved.
 //
-//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software
-//  and associated documentation files (the "Software"), to deal in the Software without
-//  restriction, including without limitation the rights to use, copy, modify, merge, publish,
-//  distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
-//  Software is furnished to do so, subject to the following conditions:
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+//  except in compliance with the License. You may obtain a copy of the License at
 //
-//      The above copyright notice and this permission notice shall be included in all copies or
-//      substantial portions of the Software.
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-//  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
-//  BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-//  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//  unless required by applicable law or agreed to in writing, software distributed under the
+//  license is distributed on an "as is" basis, without warranties or conditions of any kind,
+//  either express or implied. see the license for the specific language governing permissions
+//  and limitations under the license.
 //
-///////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
 #import <Foundation/Foundation.h>
 #import "PAGComposition.h"
-#import "PAGText.h"
 #import "PAGFont.h"
 #import "PAGImage.h"
+#import "PAGText.h"
 #import "PAGTimeStretchMode.h"
 
-@interface PAGFile : PAGComposition
+PAG_API @interface PAGFile : PAGComposition
 /**
  * The maximum tag level current SDK supports.
  */
 + (uint16_t)MaxSupportedTagLevel;
 
 /***
- * Load a pag file from the specified path, return null if the file does not exist or the data is not a pag file.
- * Note: All PAGFiles loaded by the same path share the same internal cache. The internal cache is alive until all PAGFiles are released. Use '[PAGFile Load:size:]' instead if you don't want to load a PAGFile from the intenal caches.
+ * Load a pag file from the specified path, return null if the file does not exist or the data is
+ * not a pag file. Note: All PAGFiles loaded by the same path share the same internal cache. The
+ * internal cache is alive until all PAGFiles are released. Use '[PAGFile Load:size:]' instead if
+ * you don't want to load a PAGFile from the intenal caches.
  */
-+ (PAGFile *)Load:(NSString *)path;
++ (PAGFile*)Load:(NSString*)path;
 
 /**
  *  Load a pag file from byte data, return null if the bytes is empty or it's not a valid pag file.
  */
-+ (PAGFile *)Load:(const void *)bytes size:(size_t)length;
++ (PAGFile*)Load:(const void*)bytes size:(size_t)length;
 
 /**
  * The tag level this pag file requires.
@@ -68,35 +65,35 @@
 /**
  * The path string of this file, returns empty string if the file is loaded from byte stream.
  */
-- (NSString *)path;
+- (NSString*)path;
 
 /**
  * Get a text data of the specified index. The index ranges from 0 to numTexts - 1.
  * Note: It always returns the default text data.
  */
-- (PAGText *)getTextData:(int)editableTextIndex;
+- (PAGText*)getTextData:(int)editableTextIndex;
 
 /**
- * Replace the text data of the specified index. The index ranges from 0 to PAGFile.numTexts - 1. Passing in null
- * for the textData parameter will reset it to default text data.
+ * Replace the text data of the specified index. The index ranges from 0 to PAGFile.numTexts - 1.
+ * Passing in null for the textData parameter will reset it to default text data.
  */
-- (void)replaceText:(int)editableTextIndex data:(PAGText *)value;
+- (void)replaceText:(int)editableTextIndex data:(PAGText*)value;
 
 /**
- * Replace the image data of the specified index. The index ranges from 0 to PAGFile.numImages - 1. Passing in null
- * for the image parameter will reset it to default image data.
+ * Replace the image data of the specified index. The index ranges from 0 to PAGFile.numImages - 1.
+ * Passing in null for the image parameter will reset it to default image data.
  */
-- (void)replaceImage:(int)editableImageIndex data:(PAGImage *)value;
+- (void)replaceImage:(int)editableImageIndex data:(PAGImage*)value;
 
 /**
  * Return an array of layers by specified editable index and layer type.
  */
-- (NSArray<PAGLayer *> *)getLayersByEditableIndex:(int)index layerType:(PAGLayerType)type;
+- (NSArray<PAGLayer*>*)getLayersByEditableIndex:(int)index layerType:(PAGLayerType)type;
 
 /**
- * Indicate how to stretch the original duration to fit target duration when file's duration is changed.
- * The default value is PAGTimeStretchMode::Repeat.
-*/
+ * Indicate how to stretch the original duration to fit target duration when file's duration is
+ * changed. The default value is PAGTimeStretchMode::Repeat.
+ */
 - (PAGTimeStretchMode)timeStretchMode;
 
 /**
@@ -105,13 +102,15 @@
 - (void)seTimeStretchMode:(PAGTimeStretchMode)value;
 
 /**
- * Set the duration of this PAGFile. Passing a value less than or equal to 0 resets the duration to its default value.
+ * Set the duration of this PAGFile. Passing a value less than or equal to 0 resets the duration to
+ * its default value.
  */
 - (void)setDuration:(int64_t)duration;
 
 /**
- * Make a copy of the original file, any modification to current file has no effect on the result file.
+ * Make a copy of the original file, any modification to current file has no effect on the result
+ * file.
  */
-- (PAGFile *)copyOriginal;
+- (PAGFile*)copyOriginal;
 
 @end
--- framework/libpag.xcframework/ios-arm64_armv7/libpag.framework/Headers/PAGLayer.h
+++ framework/libpag.xcframework/ios-arm64_armv7/libpag.framework/Headers/PAGLayer.h
@@ -1,43 +1,39 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//  The MIT License (MIT)
+//  Tencent is pleased to support the open source community by making libpag available.
 //
-//  Copyright (c) 2016-present, Tencent. All rights reserved.
+//  Copyright (C) 2021 THL A29 Limited, a Tencent company. All rights reserved.
 //
-//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software
-//  and associated documentation files (the "Software"), to deal in the Software without
-//  restriction, including without limitation the rights to use, copy, modify, merge, publish,
-//  distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
-//  Software is furnished to do so, subject to the following conditions:
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+//  except in compliance with the License. You may obtain a copy of the License at
 //
-//      The above copyright notice and this permission notice shall be included in all copies or
-//      substantial portions of the Software.
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-//  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
-//  BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-//  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//  unless required by applicable law or agreed to in writing, software distributed under the
+//  license is distributed on an "as is" basis, without warranties or conditions of any kind,
+//  either express or implied. see the license for the specific language governing permissions
+//  and limitations under the license.
 //
-///////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
+#import <CoreGraphics/CoreGraphics.h>
 #import <Foundation/Foundation.h>
 #import "PAGMarker.h"
 
 typedef NS_ENUM(NSInteger, PAGLayerType) {
-    PAGLayerTypeUnknown,
-    PAGLayerTypeNull,
-    PAGLayerTypeSolid,
-    PAGLayerTypeText,
-    PAGLayerTypeShape,
-    PAGLayerTypeImage,
-    PAGLayerTypePreCompose,
+  PAGLayerTypeUnknown,
+  PAGLayerTypeNull,
+  PAGLayerTypeSolid,
+  PAGLayerTypeText,
+  PAGLayerTypeShape,
+  PAGLayerTypeImage,
+  PAGLayerTypePreCompose,
 };
 
 @class PAGComposition;
 @class PAGFile;
 
-@interface PAGLayer : NSObject
+PAG_API @interface PAGLayer : NSObject
 
 /**
  * Returns the type of layer.
@@ -47,12 +43,12 @@ typedef NS_ENUM(NSInteger, PAGLayerType) {
 /**
  * Returns the name of the layer.
  */
-- (NSString *)layerName;
+- (NSString*)layerName;
 
 /**
  * A matrix object containing values that alter the scaling, rotation, and translation of the layer.
- * Altering it does not change the animation matrix, and it will be concatenated to current animation matrix for
- * displaying.
+ * Altering it does not change the animation matrix, and it will be concatenated to current
+ * animation matrix for displaying.
  */
 - (CGAffineTransform)matrix;
 
@@ -64,7 +60,8 @@ typedef NS_ENUM(NSInteger, PAGLayerType) {
 - (void)resetMatrix;
 
 /**
- * The final matrix for displaying, it is the combination of the matrix property and current matrix from animation.
+ * The final matrix for displaying, it is the combination of the matrix property and current matrix
+ * from animation.
  */
 - (CGAffineTransform)getTotalMatrix;
 
@@ -76,30 +73,30 @@ typedef NS_ENUM(NSInteger, PAGLayerType) {
 - (void)setVisible:(BOOL)visible;
 
 /**
- * Ranges from 0 to PAGFile.numTexts - 1 if the layer type is text, or from 0 to PAGFile.numImages -1 if the
- * layer type is image, otherwise returns -1.
+ * Ranges from 0 to PAGFile.numTexts - 1 if the layer type is text, or from 0 to PAGFile.numImages
+ * -1 if the layer type is image, otherwise returns -1.
  */
 - (NSInteger)editableIndex;
 
 /**
  * Indicates the PAGComposition instance that contains this PAGLayer instance.
  */
-- (PAGComposition *)parent;
+- (PAGComposition*)parent;
 
 /**
  * Returns the markers of current PAGLayer.
  */
-- (NSArray<PAGMarker *> *)markers;
+- (NSArray<PAGMarker*>*)markers;
 
 /**
- * Converts the time from the PAGLayer's (local) timeline to the PAGSurface (global) timeline. The time is in
- * microseconds.
+ * Converts the time from the PAGLayer's (local) timeline to the PAGSurface (global) timeline. The
+ * time is in microseconds.
  */
 - (int64_t)localTimeToGlobal:(int64_t)localTime;
 
 /**
- * Converts the time from the PAGSurface (global) to the PAGLayer's (local) timeline timeline. The time is in
- * microseconds.
+ * Converts the time from the PAGSurface (global) to the PAGLayer's (local) timeline timeline. The
+ * time is in microseconds.
  */
 - (int64_t)globalToLocalTime:(int64_t)globalTime;
 
@@ -114,7 +111,8 @@ typedef NS_ENUM(NSInteger, PAGLayerType) {
 - (float)frameRate;
 
 /**
- * The start time of the layer in microseconds, indicates the start position of the visible range. It could be a negative value.
+ * The start time of the layer in microseconds, indicates the start position of the visible range.
+ * It could be a negative value.
  */
 - (int64_t)startTime;
 
@@ -124,8 +122,8 @@ typedef NS_ENUM(NSInteger, PAGLayerType) {
 - (void)setStartTime:(int64_t)time;
 
 /**
- * The current time of the layer in microseconds, the layer is invisible if currentTime is not in the visible range
- * (startTime <= currentTime < startTime + duration).
+ * The current time of the layer in microseconds, the layer is invisible if currentTime is not in
+ * the visible range (startTime <= currentTime < startTime + duration).
  */
 - (int64_t)currentTime;
 
@@ -140,24 +138,25 @@ typedef NS_ENUM(NSInteger, PAGLayerType) {
 - (double)getProgress;
 
 /**
- * Set the progress of play position, the value ranges from 0.0 to 1.0. A value of 0.0 represents the
- * frame at startTime. A value of 1.0 represents the frame at the end of duration.
+ * Set the progress of play position, the value ranges from 0.0 to 1.0. A value of 0.0 represents
+ * the frame at startTime. A value of 1.0 represents the frame at the end of duration.
  */
 - (void)setProgress:(double)value;
 
 /**
  * Returns trackMatte layer of this layer.
  */
-- (PAGLayer *)trackMatteLayer;
+- (PAGLayer*)trackMatteLayer;
 
 /**
- * Returns a rectangle that defines the original area of the layer, which is not transformed by the matrix.
+ * Returns a rectangle that defines the original area of the layer, which is not transformed by the
+ * matrix.
  */
 - (CGRect)getBounds;
 
 /**
- * Indicate whether this layer is excluded from parent's timeline. If set to true, this layer's current time
- * will not change when parent's current time changes.
+ * Indicate whether this layer is excluded from parent's timeline. If set to true, this layer's
+ * current time will not change when parent's current time changes.
  */
 - (BOOL)excludedFromTimeline;
 
@@ -165,6 +164,5 @@ typedef NS_ENUM(NSInteger, PAGLayerType) {
  * Set the excludedFromTimeline flag of this layer.
  */
 - (void)setExcludedFromTimeline:(BOOL)value;
-
 
 @end
