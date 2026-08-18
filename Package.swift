f47632d239210144d548713290847e4220a5728f
libpag <libpag@tencent.com>
2026-08-18 11:48:13 +0800

    Push From Landun: update SDK 4.4.68

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "libpag",
-            url: "https://github.com/libpag/pag-ios/releases/download/4.4.67/libpag_4.4.67_ios_arm64_x86_64.zip",
-            checksum: "20d00986a5f30e1cff3f21880a53c94a5ebe1de4083129a8ce5c8588f09372f2")
+            url: "https://github.com/libpag/pag-ios/releases/download/4.4.68/libpag_4.4.68_ios_arm64_x86_64.zip",
+            checksum: "4690af89a021ae17664fd56ab1c354013eebd7a43bbb8a7dcda18604f1d3bd0a")
     ]
 )
